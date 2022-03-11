3fce960394e16fe7609db83302a1f21d0d21a5da
yoonlight <yoonlight12@gmail.com>
2022-03-11 14:01:51 +0900

    feat: add mongodb

--- server/main.py
+++ server/main.py
@@ -3,7 +3,12 @@ from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+from server.database import get_database
+
 app = FastAPI()
+
+dbname = get_database()
+collection_name = dbname["user_1_items"]
 
 class Item(BaseModel):
     name: str
@@ -17,8 +22,12 @@ def read_root():
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+    item = Item(**collection_name.find_one({"item_id": item_id}))
+    return item
 
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
+    result = item.dict()
+    result["item_id"] = item_id
+    collection_name.insert_one(result)
     return {"item_name": item.name, "item_id": item_id}
